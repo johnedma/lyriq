2fe4e57afbe22b36baefe8d698a96c313e176088
johnedma <johned.matt@gmail.com>
2020-02-12 01:28:02 -0600

    Update Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,13 +6,14 @@ function Question() {
     const {currentQuestion, questions} = state;
     const question = questions[currentQuestion];
     return(
-       <h1 style={{
-         margin: `1em`, padding: `1em`, background: `white`,
-    fontStyle: `italic`,
-    border: `0`,
-  borderRadius: `0`,
-    boxShadow: `none`,
+       <h1 className="question" style={{
+      background: `white`,
+      fontStyle: `italic`,
+      border: `0`,
+      borderRadius: `0`,
+      boxShadow: `none`,
   }}>"{question.question}"</h1>
 );
 }
 export default Question;
+
